2b894e10432eb7a7196ad3d8c41cbe03ecfcdf96
Nimor111 <georgi.bojinov@hotmail.com>
2020-02-21 00:15:53 +0200

    Set zathura font

--- zathura/default.nix
+++ zathura/default.nix
@@ -6,13 +6,14 @@
       enable = true;
       options = {
         zoom-step = 20;
-	scroll-step = 80;
-	selection-clipboard = "clipboard";
-	incremental-search = true;
+        scroll-step = 80;
+        selection-clipboard = "clipboard";
+        incremental-search = true;
+        font = "monospace normal 16";
       };
       extraConfig = ''
-	map <C-i> zoom in
-	map <C-l> zoom out
+        map <C-i> zoom in
+        map <C-l> zoom out
       '';
     };
   };
