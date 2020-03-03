9ce009ed1de2f772d4e5377d7e1ba1b763759326
Nimor111 <georgi.bojinov@hotmail.com>
2020-03-03 18:04:07 +0200

    Neovim python setup

--- neovim/default.nix
+++ neovim/default.nix
@@ -9,22 +9,8 @@ in
         enable = true;
         viAlias = true;
         vimAlias = true;
-        plugins = [
-          plugins.vim-nix
-          plugins.vim-multiple-cursors
-          plugins.vim-highlightedyank
-          plugins.vim-auto-save
-          plugins.vim-airline
-          plugins.vim-airline-themes
-          plugins.vim-elixir
-          plugins.nerdcommenter
-          plugins.nerdtree
-          plugins.ctrlp
-          plugins.gruvbox
-          plugins.haskell-vim
-          plugins.ghcid
-        ];
-        extraConfig = ''
+        configure = {
+          customRC = ''
           " spaces instead of tabs
           set expandtab
 
@@ -78,7 +64,61 @@ in
 
           " map turning off highlighting in searches
           nmap <leader>f :set nohlsearch<CR>
-        '';
+
+          " jedi-vim settings
+          let g:jedi#auto_initialization = 1
+          let g:jedi#show_call_signatures = '2'
+
+          " since we're using deoplete for completions
+          let g:jedi#completions_enabled = 0
+
+          let g:jedi#goto_command = '<leader>d'
+          let g:jedi#goto_assignments_command = '<leader>g'
+          let g:jedi#goto_definitions_command = '<leader>gg'
+          let g:jedi#documentation_command = 'K'
+          let g:jedi#usages_command = '<leader>n'
+          let g:jedi#completions_command = '<C-Space>'
+          let g:jedi#rename_command = '<leader>r'
+
+          " deoplete
+          let g:deoplete#enable_at_startup = 1
+
+          " python linter
+          let g:neomake_python_enabled_makers = ['flake8']
+
+          " automatic linting
+          autocmd BufEnter * call neomake#configure#automake('nrwi', 100)
+
+          " open quickfix window on neomake errors
+          let g:neomake_open_list = 2
+          '';
+          packages.myVimPackage = {
+            start = [
+              plugins.vim-nix
+              plugins.vim-multiple-cursors
+              plugins.vim-highlightedyank
+              plugins.vim-auto-save
+              plugins.vim-airline
+              plugins.vim-airline-themes
+              plugins.vim-elixir
+              plugins.nerdcommenter
+              plugins.nerdtree
+              plugins.ctrlp
+              plugins.gruvbox
+              plugins.haskell-vim
+              plugins.ghcid
+              plugins.neomake
+              plugins.supertab
+              plugins.deoplete-nvim
+
+              # python plugins
+              plugins.deoplete-jedi
+              plugins.jedi-vim
+            ];
+            opt = [ ];
+          };
+        };
+        extraPython3Packages = (ps: with ps; [ jedi pynvim ]);
       };
     };
   }
