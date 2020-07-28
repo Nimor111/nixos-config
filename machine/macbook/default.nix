8aaa2c78a49e757294b93be58129c0318ecee947
Georgi Bozhinov <g.bozhinov@ocado.com>
2020-07-28 10:10:16 +0300

    Fix neuron package import

--- machine/macbook/default.nix
+++ machine/macbook/default.nix
@@ -61,7 +61,7 @@ in
       let
         neuronSrc = builtins.fetchTarball https://github.com/srid/neuron/archive/master.tar.gz;
       in
-        import neuronSrc
+        import neuronSrc {}
     )
   ];
 
